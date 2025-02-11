71bbee895b207c3953366c00bf96b9ffeed1dc2a
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-11 13:02:03 +0800

    feat: create CoreModule to manage shared components and services

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { CoreModule } from '../../core/core.module';
+import { InputComponent } from "../../components/input/input.component";
 
 @Component({
   selector: 'app-login',
-  imports: [CoreModule],
+  imports: [CoreModule, InputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  constructor() {}
+
+  tapLogin() {
+    console.log('Login tapped');
+  }
+}
