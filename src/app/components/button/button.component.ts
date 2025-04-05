6e54034077ae3f75ac322d144fda162c7718bac8
AxelanO7 <jeremia123.jm@gmail.com>
2025-04-05 22:24:24 +0800

    ✨ feat(button): add customizable button component
    - introduce customizable button component with dynamic styles
    - allow setting background color, text color, and custom classes
    - add default styling for consistent look and feel

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './button.component.css',
 })
 export class ButtonComponent {
-  constructor() {}
+  defaultClass =
+    'font-bold py-2 px-4 rounded shadow-md transition duration-300 ease-in-out transform hover:scale-105';
+  @Input() customClass: string = '';
+  @Input() bgColor: string = 'bg-blue-400';
+  @Input() textColor: string = 'text-white';
+  @Input() handleOnClick: () => void = () => {};
 }
